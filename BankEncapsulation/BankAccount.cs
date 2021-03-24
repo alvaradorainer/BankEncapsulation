a7042fc0354905f7fbefaf399cb5e2df53b4e9f9
Rainer Alvarado <alvaradorainer@gmail.com>
2021-03-23 20:57:43 -0400

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        // Field
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account!");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
